f502eea0e1556362a2ea778126dd33bea45b3a46
ahmadreza <ah.tahmasbi@alibaba.ir>
2023-10-28 13:15:21 +0330

    feat: implement login page

--- of-login/src/components/HelloWorld.vue
+++ of-login/src/components/HelloWorld.vue
@@ -1,75 +1,51 @@
 <template>
-  <v-container class="fill-height">
-    <v-responsive class="align-center text-center fill-height">
-      <v-img height="300" src="@/assets/logo.svg" />
+  <form @submit.prevent="submit">
+    <v-text-field
+      v-model="email.value.value"
+      :error-messages="email.errorMessage.value"
+      label="E-mail"
+    ></v-text-field>
 
-      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
-
-      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
-
-      <div class="py-14" />
-
-      <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            href="https://vuetifyjs.com/components/all/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-view-dashboard"
-              size="large"
-              start
-            />
-
-            Components
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            color="primary"
-            href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            target="_blank"
-            variant="flat"
-          >
-            <v-icon
-              icon="mdi-speedometer"
-              size="large"
-              start
-            />
-
-            Get Started
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            href="https://community.vuetifyjs.com/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-account-group"
-              size="large"
-              start
-            />
-
-            Community
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-responsive>
-  </v-container>
+    <v-text-field
+      v-model="password.value.value"
+      :error-messages="password.errorMessage.value"
+      label="password"
+      type="password"
+    ></v-text-field>
+    <p>{{ serverError }}</p>
+    <v-btn class="me-4" type="submit"> submit </v-btn>
+  </form>
 </template>
-
 <script setup lang="ts">
-  //
+import { useField, useForm } from "vee-validate";
+import axios from "axios";
+import { ref } from "Vue";
+const { handleSubmit, handleReset } = useForm({
+  validationSchema: {
+    email(value) {
+      if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true;
+
+      return "Must be a valid e-mail.";
+    },
+  },
+});
+
+const email = useField("email");
+const password = useField("password");
+const serverError = ref("");
+interface loginData {
+  email: string;
+  password: string;
+}
+
+const submit = handleSubmit((vals: loginData) => {
+  axios
+    .post("https://dummyjson.com/products/add", vals)
+    .then((res) => {
+      alert("success");
+    })
+    .catch((e) => {
+      serverError.value = e.message;
+    });
+});
 </script>
