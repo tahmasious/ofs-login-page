be55b96271c9e53dc2ccc748c8631d63b07aa43d
ahmadreza <ah.tahmasbi@alibaba.ir>
2023-10-28 13:26:59 +0330

    fix: change component name

--- of-login/src/App.vue
+++ of-login/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <LoginPage />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
+import LoginPage from "@/components/LoginPage.vue";
 </script>
